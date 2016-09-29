e12127cfe528a830d97b4c215fa420ddf418f7dd
Jeff Brown <jeffbrown@google.com>
2016-09-29 15:25:34 -0700

    Support ignoring the startup config and running a specific app.
    Change-Id: I3ec7d74af312e257ff375de77c0df4c99bfe162c

--- application_manager/main.cc
+++ application_manager/main.cc
@@ -31,15 +31,29 @@ static void LoadStartupConfig(mojo::StartupConfig* config) {
 }
 
 int main(int argc, char** argv) {
-  mojo::StartupConfig config;
-  LoadStartupConfig(&config);
+  auto args = ftl::CommandLineFromArgcArgv(argc, argv);
 
-  mojo::ApplicationManager manager(config.TakeArgsFor());
-  mojo::CommandListener command_listener(&manager);
+  std::vector<std::string> initial_apps;
+  mojo::ApplicationArgs args_for;
+
+  const auto& positional_args = args.positional_args();
+  if (positional_args.size() > 0) {
+    // TODO(alhaad): This implementation passes all the command-line arguments
+    // to the initial application. Having an '--args-for' option is desirable.
+    std::string initial_app = positional_args[0];
+    initial_apps.push_back(initial_app);
+    args_for[initial_app] = positional_args;
+  } else {
+    mojo::StartupConfig config;
+    LoadStartupConfig(&config);
+    initial_apps = config.TakeInitialApps();
+    args_for = config.TakeArgsFor();
+  }
 
   mtl::MessageLoop message_loop;
+  mojo::ApplicationManager manager(std::move(args_for));
+  mojo::CommandListener command_listener(&manager);
 
-  std::vector<std::string> initial_apps = config.TakeInitialApps();
   if (!initial_apps.empty()) {
     message_loop.task_runner()->PostTask([&manager, &initial_apps] {
       for (const std::string& app : initial_apps)
